2235afeef4e95ebad5427cfbac225a527dcb7860
moon <113469325+Moon0256@users.noreply.github.com>
2024-06-22 15:52:31 +0530

    problemset prac

---
+++ ProblemsetPrac/berland.java
@@ -0,0 +1,20 @@
+package ProblemsetPrac;
+
+import java.util.Scanner;
+
+public class berland {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int t = input.nextInt();
+        for(int i = 0; i<t;i++){
+            int n = input.nextInt();
+            if(n==1||n==4||n==7){
+                System.out.println(1);
+            }else if(n==2){
+                System.out.println(2);
+            }else{
+                System.out.println(0);
+            }
+        }
+    }
+}
