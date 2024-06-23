866a9f5d0822ccd1b7f5d39ff3a51c9779d1ac88
moon <113469325+Moon0256@users.noreply.github.com>
2024-06-24 00:11:53 +0530

    Codeforces Round 954 Div 3

--- CodeforcesRound954Div3/q4.java
+++ CodeforcesRound954Div3/q4.java
@@ -1,11 +1,13 @@
+package CodeforcesRound954Div3;
 import java.util.Scanner;
 
-public class q2 {
+public class q4 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int t = input.nextInt();
         for(int g = 0; g<t; g++){
-            
+            int n = input.nextInt();
+            String s = input.nextLine();
         }
     }
     
