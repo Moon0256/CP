f10f9e7031e7d513b280bb75741c62c49f143ccb
moon <113469325+Moon0256@users.noreply.github.com>
2024-06-25 16:45:33 +0530

    README updates

---
+++ q1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class q1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int t = input.nextInt();
+        for(int g = 0; g<t;g++){
+            
+        }
+    }
+    
+}
