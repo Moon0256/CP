2bf651529d715c21dc70b306b4e9aa33a82b7fbd
moon <113469325+Moon0256@users.noreply.github.com>
2024-07-13 19:07:59 +0530

    Codeforces Round 957 Div 3

--- CodeforcesRound957Div3/q4.cpp
+++ CodeforcesRound957Div3/q4.cpp
@@ -14,8 +14,30 @@ int main(){
         cin >> n >> m >> k;
         string s;
         cin >> s;
-        for(int i =0; i<m; i++){
-            
+        s = "L"+s+ "L";
+        int dp[n+2];
+        dp[n+1]=0; // this is at the end, so swim distance = 0
+
+        for(int i = n; i>=0; i--){
+            if(s[i]=='L'){
+                int mini = 1000000;
+                for(int j = 1; j<=m; j++){
+                    if(i+j<n+2){
+                        mini = min(mini,dp[i+j]);
+                    }
+                }
+                dp[i]=mini;
+            }else if(s[i]=='W'){
+                dp[i]=dp[i+1]+1;
+            }else{
+                dp[i]=1000000;
+            }
+        }
+
+        if(dp[0]<=k){
+            YES;
+        }else{
+            NO;
         }
     }
 }
